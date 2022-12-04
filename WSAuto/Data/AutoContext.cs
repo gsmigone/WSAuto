516eba907051c174e82f57b66f7196e649ede22b
gaston migone <gsmigone@gmail.com>
2022-12-03 22:35:26 -0300

    Agregar archivos de proyecto.

---
+++ WSAuto/Data/AutoContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WSAuto.Models;
+
+namespace WSAuto.Data
+{
+    public class AutoContext : DbContext
+    {
+        public AutoContext() { }
+
+        public AutoContext(DbContextOptions<AutoContext> options) : base(options) { }
+
+        public virtual DbSet<Auto> Autos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<Auto>().ToTable("Vehiculo");
+
+            modelBuilder.Entity<Auto>().Property(e => e.Marca).IsRequired().HasColumnType("varchar(50)");
+            modelBuilder.Entity<Auto>().Property(e => e.Modelo).IsRequired().HasColumnType("varchar(50)");
+            modelBuilder.Entity<Auto>().Property(e => e.Color).IsRequired().HasColumnType("varchar(50)");
+            modelBuilder.Entity<Auto>().Property(e => e.Precio).IsRequired().HasColumnType("money");
+
+        }
+    }
+}
---
+++ WSAuto/Models/Auto.cs
@@ -0,0 +1,18 @@
+﻿namespace WSAuto.Models
+{
+    public class Auto
+    {
+        
+        public int Id { get; set; }
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public string Color { get; set; }
+        public decimal Precio { get; set; }
+
+        public Auto()
+        {
+
+        }
+
+    }
+}
